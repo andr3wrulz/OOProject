94035019333bf957c502b01efc58b7b7a8b51209
nicolas lopez <nicolas lopez>
2017-05-25 16:10:22 -0400

    Menu added

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	public void newGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void saveGameBtn(string saveState)
+    {
+        //add stuff
+    }
+
+    public void loadGameBtn(int loadState) 
+    {
+        //add stuff
+    }
+
+    public void enterDungeonBtn(string dungeon) //input Player player as a parameter
+    {
+        SceneManager.LoadScene(dungeon);
+    }
+
+    public void enterStoreBtn(string store) //add param player.gold
+    {
+        SceneManager.LoadScene(store);
+    }
+
+    public void returnToMainMenuBtn(string menu)
+    {
+        SceneManager.LoadScene(menu);
+    }
+
+    public void characterBtn(string characterSheet)
+    {
+        SceneManager.LoadScene(characterSheet);
+    }
+}
