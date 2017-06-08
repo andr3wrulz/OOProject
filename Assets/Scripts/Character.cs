b1aaf76225f582ac379bf88eb13e851f3285f797
Khoi nguyen <knguyen83@mail.valenciacollege.edu>
2017-06-08 10:23:02 -0400

    Add files via upload
    Base character class

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character {
+
+	private string characterDescription; 
+
+	private int strength; 
+	private int endurance; 
+	private int intellect; 
+
+
+	public int getStrength ()
+	{
+		return this.strength; 
+	}
+
+	public int getEndurance ()
+	{
+		return this.strength; 
+	}
+
+	public int getIntellect ()
+	{
+		return this.strength; 
+	}
+}
+	
