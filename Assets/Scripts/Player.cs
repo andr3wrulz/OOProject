8ccc0c0f30d789df810aa2ba83fb99ce5a591603
Nicolas Lopez <nicolaslopezbravo@gmail.com>
2017-06-20 21:16:45 -0400

    merged playerclass and player, wall bug 2befixed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Moving
 {
     /* Room settings for player */
 	IntVector2 currentRoom;
@@ -16,8 +16,17 @@ public class Player : MonoBehaviour
     int tempHealth;
     int attackPower;
 
-	// Use this for initialization
-	void Start ()
+    // for testing
+    int count;
+    //-----
+
+    int speed = 10;
+
+    // This is used for attacking  by bow. 
+    float range = 4f;
+
+    // Use this for initialization
+    void Start ()
     {
 		currentRoom = GameControl.control.startRoom;
 
@@ -35,6 +44,7 @@ public class Player : MonoBehaviour
         // attackPower = ???;
 
         // Player attack animation 
+        base.Start();
         animator = GetComponent<Animator>();
     }
 	
@@ -51,6 +61,41 @@ public class Player : MonoBehaviour
 
         // Is player being attacked?
         GetHit();
+
+        /* Player moving code */
+        int x, y;
+
+        x = (int)Input.GetAxisRaw("Horizontal");
+        y = (int)Input.GetAxisRaw("Vertical");
+
+        if (x != 0 || y != 0)
+        {
+            // This prevents diagonal move. 
+            if (x != 0)
+            {
+                y = 0;
+            }
+
+            Vector2 start = transform.position;
+            Vector2 end = start + new Vector2(x, y);
+            Vector2 newPosition = Vector2.MoveTowards(start, end, speed * Time.deltaTime);
+
+            RaycastHit2D hitSword;
+            RaycastHit2D hitBow;
+
+            Debug.DrawLine(start, end, Color.red);
+
+            if (IsObstacle(x, y, range, out hitSword, out hitBow))
+            {
+                Debug.Log("Encounter obstacle " + count++);
+
+                Debug.Log(hitSword.transform);
+                Debug.Log(hitBow.transform);
+            }else
+            {
+                ToMove(newPosition);
+            }
+        }
     }
 
     void Attack()
